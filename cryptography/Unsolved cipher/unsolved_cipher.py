4491e1d5e154a2d75b732c009d7405824bbe3546
Euthanazepam <euthanazepam@protonmail.com>
2024-07-06 20:02:29 +0000

    #add #cryptography - unsolved cipher

---
+++ cryptography/Unsolved cipher/unsolved_cipher.py
@@ -0,0 +1,99 @@
+import requests
+
+from zipfile import ZipFile
+
+base_url = "https://codeby.games"
+path = "game_api/files/download"
+folder = "parts_92e6a4b9-1899-4584-9780-2312da201665_data"
+filename = "notsolvedcipher"
+filetype = "zip"
+
+
+def download_zip() -> None:
+    """
+    Downloads a zip file from the task page to the current directory.
+    """
+
+    url = f"{base_url}/{path}?folder={folder}&name={filename}&type={filetype}"
+
+    response = requests.get(url=url)
+
+    try:
+        with open(f"Unsolved cipher/{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+    except FileNotFoundError:
+        with open(f"{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+
+
+def unzip() -> str:
+    """
+    Unpacks a zip file into the current directory.
+    :return: Name of file
+    """
+
+    download_zip()
+
+    try:
+        with ZipFile(f"Unsolved cipher/{filename}.{filetype}") as zf:
+            zf.extractall(path="Unsolved cipher/")
+            return zf.namelist()[0]
+    except FileNotFoundError:
+        with ZipFile(f"{filename}.{filetype}") as zf:
+            zf.extractall(path=".")
+            return zf.namelist()[0]
+
+
+def vigenere_decrypt(cipher_text, key) -> str:
+    """
+    Decryption function for the Vigenère cipher.
+    Source: https://thepythoncode.com/article/implementing-the-vigenere-cipher-in-python
+
+    :param cipher_text: Encrypted text
+    :param key: Encryption key
+    :return: Decrypted text
+    """
+
+    decrypted_text = ''
+
+    key_repeated = (key * (len(cipher_text) // len(key))) + key[:len(cipher_text) % len(key)]
+
+    for i in range(len(cipher_text)):
+        if cipher_text[i].isalpha():
+            shift = ord(key_repeated[i].upper()) - ord('A')
+            if cipher_text[i].isupper():
+                decrypted_text += chr((ord(cipher_text[i]) - shift - ord('A')) % 26 + ord('A'))
+            else:
+                decrypted_text += chr((ord(cipher_text[i]) - shift - ord('a')) % 26 + ord('a'))
+        else:
+            decrypted_text += cipher_text[i]
+
+    return decrypted_text
+
+
+def get_flag() -> str:
+    """
+    https://codeby.games/en/categories/cryptography/92e6a4b9-1899-4584-9780-2312da201665
+    """
+
+    file_name = unzip()
+
+    try:
+        with open(f"Unsolved cipher/{file_name}", "r") as f:
+            not_solved_cipher = f.readlines()
+    except FileNotFoundError:
+        with open(f"{file_name}", "r") as f:
+            not_solved_cipher = f.readlines()
+
+    cipher_text = not_solved_cipher[0][6:].replace('{', '').replace('}', '').strip('\n')
+    key = not_solved_cipher[1][6:].strip('\n')
+
+    plain_text = vigenere_decrypt(cipher_text, key)
+
+    flag = plain_text[:6] + '{' + plain_text[6:] + '}'
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
