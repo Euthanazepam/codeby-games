b8e50d5afcb126c299f95d7f57a87dc542d4ea88
Euthanazepam <euthanazepam@protonmail.com>
2024-07-15 17:56:50 +0000

    #add #steganography - omnipresent

---
+++ Steganography/Omnipresent/omnipresent.py
@@ -0,0 +1,67 @@
+from base64 import b64decode
+from requests import get
+from zipfile import ZipFile
+
+base_url = "https://codeby.games"
+path = "game_api/files/download"
+folder = "parts_a6c02b63-e625-418f-8a94-82afe20a2d8b_data"
+filename = "Omniscient"
+filetype = "zip"
+
+
+def download_zip() -> None:
+    """
+    Downloads a zip file from the task page to the current directory.
+    """
+
+    url = f"{base_url}/{path}?folder={folder}&name={filename}&type={filetype}"
+
+    response = get(url=url)
+
+    try:
+        with open(f"Omnipresent/{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+    except FileNotFoundError:
+        with open(f"{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+
+
+def unzip() -> None:
+    """
+    Unpacks a zip file into the current directory.
+    """
+
+    download_zip()
+
+    try:
+        with ZipFile(f"Omnipresent/{filename}.{filetype}") as zf:
+            zf.extractall(path="Omnipresent/")
+    except FileNotFoundError:
+        with ZipFile(f"{filename}.{filetype}") as zf:
+            zf.extractall(path=".")
+
+
+def get_flag() -> str:
+    """
+    https://codeby.games/en/categories/steganography/a6c02b63-e625-418f-8a94-82afe20a2d8b
+    """
+
+    unzip()
+
+    """
+    References:
+        1. PNG Alpha Channel Extractor — https://onlinepngtools.com/extract-alpha-channel-from-png
+
+    ==============================================================================================
+
+    Extract alpha channel from png. It contains flag.
+    """
+
+    # Encoded the flag in base64 to avoid spoilers.
+    flag = b64decode('Q09ERUJZezRuZF9pX2M0bl9zZTN9').decode()
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
