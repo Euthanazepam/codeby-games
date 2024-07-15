3ff921971e4eff2d890476a4bc73881d27b7f64a
Euthanazepam <euthanazepam@protonmail.com>
2024-07-15 17:43:35 +0000

    #add #steganography - wide monitor

---
+++ Steganography/Wide monitor/wide_monitor.py
@@ -0,0 +1,48 @@
+from base64 import b64decode
+from requests import get
+
+base_url = "https://codeby.games"
+path = "game_api/files/download"
+folder = "parts_a1e6e8a9-0ea3-4605-880e-c7cf528d2980_data"
+filename = "file"
+filetype = "txt"
+
+
+def download_file() -> None:
+    """
+    Downloads a text file from the task page to the current directory.
+    """
+
+    url = f"{base_url}/{path}?folder={folder}&name={filename}&type={filetype}"
+
+    response = get(url=url)
+
+    try:
+        with open(f"Wide monitor/{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+    except FileNotFoundError:
+        with open(f"{filename}.{filetype}", "wb") as f:
+            f.write(response.content)
+
+
+def get_flag() -> str:
+    """
+    https://codeby.games/en/categories/steganography/a1e6e8a9-0ea3-4605-880e-c7cf528d2980
+    """
+
+    download_file()
+
+    """
+    References:
+        1. ASCII to Image Converter — https://onlinetools.com/ascii/convert-ascii-to-image
+        2. Image to ASCII — https://www.asciiart.eu/image-to-ascii
+    """
+
+    # Encoded the flag in base64 to avoid spoilers.
+    flag = b64decode('Q09ERUJZe0dPT0RfSk9CX01ZX0ZSMUVORCF9').decode()
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
